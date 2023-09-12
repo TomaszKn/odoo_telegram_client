d93b1c200c436218da0fccdf184c674a6ccd4667
Mikhail <erlit007@gmail.com>
2023-09-12 15:04:30 +0300

    Updated

--- models/extend_mail_message.py
+++ models/extend_mail_message.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, exceptions
+from odoo.exceptions import UserError
 
 
 # This module adds few new fields to 'mail.message' model
@@ -26,38 +27,52 @@ class TelegramMessage(models.Model):
     #  and call send_telegram_message()   
     @api.model
     def create(self, vals):
-        print(vals)
-        channel_id = vals.get('res_id')
-        channel = self.env['mail.channel'].browse(channel_id)
-        print(f'=======INFO(extend_mail_message.py): vals["message_type"] = {vals.get("message_type")}')
-        if channel.is_telegram:
-            print(f'=======INFO(extend_mail_message.py):channel = {channel.is_telegram}')
-
-            # Check if message_type is 'telegram' then it's incoming from Telegram
-            # So regular create() should be start, no need to post this message to Telegram again
-            if vals.get("message_type") == 'telegram' or vals.get("message_type") == 'notification':
-                print(f'=======INFO(extend_mail_message.py): vals.get("message_type") = telegram. Call regular create method')
+        try:
+            if vals.get('model') != 'mail.channel':
                 message = super().create(vals)
-                print(f'=======INFO(extend_mail_message.py): Create has been called')
                 return message
+                
+            print(vals)
+            channel_id = vals.get('res_id')
+            print(f'=======INFO 1 (extend_mail_message.py): channel_id = {channel_id}')
+            channel = self.env['mail.channel'].browse(channel_id)
+            print(f'=======INFO 2 (extend_mail_message.py): channel = {channel}')
+            print(f'=======INFO 3 (extend_mail_message.py): vals["message_type"] = {vals.get("message_type")}')
+            print(f'=======INFO 3.1 (extend_mail_message.py): channel.is_telegram = {channel.is_telegram}')
+            if channel.is_telegram and vals.get("message_type") == 'comment':
+                print(f'=======INFO 4 (extend_mail_message.py):channel = {channel.is_telegram}')
+                try:
+                    print(f'=======INFO 7 (extend_mail_message.py): TRYING TO GET telegram client')
+                    vals['telegram_dialog_id'] = channel.telegram_dialog_id
+                    print(f'=======INFO 8 (extend_mail_message.py): vals[telegram_dialog_id] = {vals["telegram_dialog_id"]}')
+                    telegram_client = self.env['telegram.client']
+                    send_result = telegram_client.send_telegram_message(vals)
+                    print(f'=======INFO 8.1   {send_result}')
+                    if not send_result.get('telegram_api'):
+                        text_message = vals.get('body')
+                        vals['body'] = 'PROBLEM: this massage was not send to a person. You need to log in intro Telegram first. ' + 'Your message: ' + '"' + text_message + '"'
+                        message = super().create(vals)
+                        return message
 
-            try:
-                print(f'=======INFO(extend_mail_message.py): TRYING TO GET telegram client')
-                vals['telegram_dialog_id'] = channel.telegram_dialog_id
-                print(f'=======INFO(extend_mail_message.py): vals[telegram_dialog_id] = {vals["telegram_dialog_id"]}')
-                telegram_client = self.env['telegram.client']
-                telegram_client.send_telegram_message(vals)
-                print(f'=======INFO(extend_mail_message.py): IS CALLED - telegram_client.send_telegram_message()')
+                    else:
+                        print(f'=======INFO 9 (extend_mail_message.py): IS CALLED - telegram_client.send_telegram_message()')
+                        message = super().create(vals)
+                        print(f'=======INFO 10 (extend_mail_message.py): MESSAGE SENT TO TG AND CREATED IN ODOO')
+                        return message
+                
+                except Exception as e:
+                    print(f'Error in send_telegram_message():')
+            else:
+                print('Channel is not a Telegram')
                 message = super().create(vals)
-                print(f'=======INFO(extend_mail_message.py): MESSAGE SENT TO TG AND CREATED IN ODOO')
                 return message
-            
-            except Exception as e:
-                print(f'Error in send_telegram_message():')
-        else:
-            print('Channel is not a Telegram')
+           
+        except Exception as e:
             message = super().create(vals)
             return message
+
+
+
 
 
     # @api.constrains('telegram_message_id', 'channel_id')
--- models/telegram_client.py
+++ models/telegram_client.py
@@ -16,11 +16,11 @@ class telegram_client(models.Model):
     api_id = fields.Integer(string='Api ID', required=True)
     api_hash = fields.Char(string='Api Hash', required=True)
     phone_number = fields.Char(string='Telegram Phone Number', required=True)
-    session_name = fields.Char(string='The name of the session file', required=True)
+    session_name = fields.Char(string='The name of the session file', required=True, readonly=True, default='telegram1')
     username = fields.Char(string='The Telegram Username', readonly=True)
     users = fields.One2many('res.users', 'telegram_client_id', string='Associated Partners', required=True)
     server_url = fields.Char(string='Remote API server', required=True)
-    auth_status = fields.Boolean(string='Auth status', default=False, readonly=True)
+    auth_status = fields.Boolean(string='Auth status')
 
 
 
@@ -28,16 +28,51 @@ class telegram_client(models.Model):
     def check_tg_auth(self):
         try:
             url = self.server_url + 'check_auth'
+            payload = {
+                "api_id": self.api_id,
+                "api_hash": self.api_hash,
+                "phone_number": self.phone_number,
+                "session_name": self.session_name,
+                "message_text": 'blank',
+                "recipient": 123
+            }
             headers = {"Content-Type": "application/json"}
-            response = requests.post(url, json={}, headers=headers)
+            response = requests.post(url, json=payload, headers=headers)
             data = response.json()
             print(f'----------{data}----------')
             if data.get('auth_status'):
-                self.auth_status = True
+                status = True
+                print(f'-----1---{status}')
             else:
-                self.auth_status = False
-        except:
-            raise UserError(f"Something is wrong. Check your credentials or connection")
+                status = False
+                print(f'-----2---{status}')
+                message = {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': ('Warning'),
+                        'message': 'FastApi server is responding but no Telegram account is connected',
+                        'sticky': False
+                    }
+                }
+                return message
+            
+        except Exception as e:
+            status = False
+            print(f'-----3---{status}')
+            message = {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': ('Warning'),
+                        'message': 'Something is wrong. Check your credentials or connection',
+                        'sticky': False
+                    }
+                }
+            self.auth_status = status
+            return message
+        print(f'-----4---{self.auth_status}')
+
 
 
 
@@ -89,21 +124,44 @@ class telegram_client(models.Model):
 
             # Check the response from FastApi server if loop started or not
             if data.get('auth_status'):
-                self.auth_status = True
+                self.write({'auth_status': True})
+                message = {
+                    'effect': {
+                        'fadeout': 'slow',
+                        'message': 'You are connected to Telegram',
+                        'type': 'rainbow_man'
+                    }
+                }
+                
+                print(f'------1-------self.auth_status = {self.auth_status}')
+                return {'type': 'ir.actions.client', 'tag': 'reload'}, message
+            else:
+                self.auth_status = False
                 message = {
                     'type': 'ir.actions.client',
                     'tag': 'display_notification',
                     'params': {
-                        'title': ('Success'),
+                        'title': ('FastApi Server is available but Telegram Session is not started. Something wrong with creadentials or authentication process.'),
                         'message': 'You are connected to Telegram',
                         'sticky': False
                     }
                 }
                 return message
-            
         except Exception as e:
-            raise UserError(f"Can't start a Telegram session loop. Check your credentials or connection" 
-                            f" to a fastapi sevice.")
+            status = False
+            message = {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': ('Warning'),
+                        'message': 'Something is wrong. Check your credentials or connection',
+                        'sticky': False
+                    }
+                }
+            self.auth_status = status
+            return message
+
+
 
     # SEND MESSAGE TO TELEGRAM
     # This method gets all the current user telegram api keys and send POST request to
@@ -129,11 +187,37 @@ class telegram_client(models.Model):
                 headers = {"Content-Type": "application/json"}
                 print(f'======= INFO(model.py): TRYING TO MAKE POST RQUEST with json = {payload}')
                 response = requests.post(url, json=payload, headers=headers)
-                response.raise_for_status()  # Raise an exception if the request was not successful (status code >= 400)
+                #response.raise_for_status()  # Raise an exception if the request was not successful (status code >= 400)
                 data = response.json()
-                print(f'======= INFO(model.py): MESSAGE SENT----------{data}----------')
+                if data.get('telegram_api'):
+                    print(f'======= INFO(model.py): MESSAGE SENT----------{data}----------')
+                    return{'telegram_api': 'OK'}
+                else:
+                    print(f'======= INFO(model.py): data.get(telegram_api) = {data.get("telegram_api")}')
+                    print(f'======= INFO(model.py): telegram_api is None')
+                    return {'telegram_api': None}
             except requests.exceptions.RequestException as e:
                 error_message = f"INFO(model.py) An error occurred while making the POST request: {e}"
                 raise exceptions.UserError(error_message)  # Raise a UserError exception to display the error in a dialog box
         else:
             raise exceptions.UserError("No Telegram credentials found for the current user.")
+        
+
+    def open_telegram_auth_wizard(self):
+        # Assuming you want to open the wizard for the current record
+        # If you want to open it for a specific record, you can pass the record's ID as a parameter
+
+        # Create an action record for the wizard view
+        action = self.env.ref('odoo_telegram_client.telegram_auth_wizard').read()[0]
+        print(f'===== WIZARD {action}')
+        # Modify the context as needed (e.g., pass additional values)
+        context = {
+            'default_phone_number': self.phone_number,  # Example field to set a default value
+            # Add more context values if needed
+        }
+
+        # Set the context in the action
+        action['context'] = context
+
+        # Open the wizard view as a new window
+        return action
